ac7912198c1e2d4e48e44d368c793d2f74b6c9bf
DinhNhat0810 <dinhnhat0810@gmail.com>
2025-06-09 18:43:12 +0700

    first commit

---
+++ config/plugins.ts
@@ -0,0 +1,39 @@
+module.exports = ({ env }) => ({
+  ckeditor: {
+    enabled: true,
+    config: {
+      editor: {
+        toolbar: {
+          items: [
+            "heading",
+            "|",
+            "bold",
+            "italic",
+            "underline",
+            "|",
+            "link",
+            "bulletedList",
+            "numberedList",
+            "|",
+            "blockQuote",
+            "codeBlock",
+            "insertTable",
+            "mediaEmbed",
+            "imageUpload",
+            "|",
+            "undo",
+            "redo",
+          ],
+        },
+        language: "en",
+        image: {
+          toolbar: [
+            "imageTextAlternative",
+            "imageStyle:full",
+            "imageStyle:side",
+          ],
+        },
+      },
+    },
+  },
+});
