57631d403bb90a0a773a7291d9b0b69f75465edc
DinhNhat0810 <dinhnhat0810@gmail.com>
2025-06-18 15:56:42 +0700

    fix host

--- src/admin/vite.config.example.ts
+++ src/admin/vite.config.example.ts
@@ -1,12 +1,9 @@
-import { mergeConfig, type UserConfig } from 'vite';
+import { mergeConfig, type UserConfig } from "vite";
 
 export default (config: UserConfig) => {
   // Important: always return the modified config
   return mergeConfig(config, {
-    resolve: {
-      alias: {
-        '@': '/src',
-      },
-    },
+    resolve: { alias: { "@": "/src" } },
+    server: { allowedHosts: ["admin.ca2.one"] },
   });
 };
