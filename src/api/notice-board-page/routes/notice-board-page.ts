320504da044b982408bd881563fc663908e7b5f1
DinhNhat0810 <dinhnhat0810@gmail.com>
2025-07-01 23:51:26 +0700

    add infor page

---
+++ src/api/notice-board-page/routes/notice-board-page.ts
@@ -0,0 +1,7 @@
+/**
+ * notice-board-page router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::notice-board-page.notice-board-page');
---
+++ src/api/notice-board-page/services/notice-board-page.ts
@@ -0,0 +1,7 @@
+/**
+ * notice-board-page service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::notice-board-page.notice-board-page');
