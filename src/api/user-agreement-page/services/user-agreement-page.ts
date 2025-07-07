c6e5b013116f20a1a2bac623e6b5a0b520cb252f
DinhNhat0810 <dinhnhat0810@gmail.com>
2025-07-08 00:18:55 +0700

    add footer page

---
+++ src/api/user-agreement-page/services/user-agreement-page.ts
@@ -0,0 +1,7 @@
+/**
+ * user-agreement-page service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::user-agreement-page.user-agreement-page');
